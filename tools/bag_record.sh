edafdd689f980c7a4c9eba05bdb4348973a373f8
RenFukatsu <fukatsuren88@gmail.com>
2021-04-15 11:39:28 +0900

    set pngs by launch

--- tools/bag_record.sh
+++ tools/bag_record.sh
@@ -5,8 +5,5 @@ if [ $# -ne 1 ] ; then
     exit 1
 fi
 
-rosparam set /camera/aligned_depth_to_color/image_raw/compressed/format png
-rosparam set /camera/aligned_depth_to_color/image_raw/compressed/png_level 6
-
 rosbag record /camera/aligned_depth_to_color/image_raw/compressed /camera/color/camera_info /camera/color/image_rect_color/compressed /theta_s/image_raw/compressed /roomba/odometry /scan -o $1
 
