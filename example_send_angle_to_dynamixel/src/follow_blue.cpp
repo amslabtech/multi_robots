0ac9a3872b53b698cf11a93cc2da856d3b66ae9c
RenFukatsu <fukatsuren88@gmail.com>
2021-04-15 12:14:47 +0900

    update exam

--- example_send_angle_to_dynamixel/src/follow_blue.cpp
+++ example_send_angle_to_dynamixel/src/follow_blue.cpp
@@ -3,18 +3,30 @@
 #include "dynamixel_angle_msgs/DynamixelAngle.h"
 #include "color_detector_msgs/TargetAngleList.h"
 
-void callback(const color_detector_msgs::TargetAngleListConstPtr &target_angles, const ros::Publisher &dynamixel_pub) {
-    for (const auto &target : target_angles->data) {
-        if (target.color != "blue" || target.cluster_num < 100) continue;
-        dynamixel_angle_msgs::DynamixelAngle msg;
-        msg.theta = target.radian;
-        dynamixel_pub.publish(msg);
+class Example {
+ public:
+    Example() {
+        dynamixel_pub = nh.advertise<dynamixel_angle_msgs::DynamixelAngle>("/angle", 1);
+        theta_s_sub = nh.subscribe("/target/angle", 1, &Example::callback, this);
     }
-}
+    void callback(const color_detector_msgs::TargetAngleListConstPtr &target_angles) {
+        for (const auto &target : target_angles->data) {
+            if (target.color != "blue" || target.cluster_num < 500) continue;
+            dynamixel_angle_msgs::DynamixelAngle msg;
+            msg.theta = target.radian;
+            dynamixel_pub.publish(msg);
+        }
+    }
+    void process() { ros::spin(); }
+ private:
+    ros::NodeHandle nh;
+    ros::Publisher dynamixel_pub;
+    ros::Subscriber theta_s_sub;
+};
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "example angle theta");
-    ros::NodeHandle nh;
-    ros::Publisher dynamixel_pub = nh.advertise<dynamixel_angle_msgs::DynamixelAngle>("/angle", 1);
-    ros::Subscriber theta_s_sub = nh.subscribe<color_detector_msgs::TargetAngleList>("/target/angle", 1, boost::bind(callback, _1, dynamixel_pub))color;
+    ros::init(argc, argv, "example_angle_theta");
+    Example exam;
+    exam.process();
+    return 0;
 }
