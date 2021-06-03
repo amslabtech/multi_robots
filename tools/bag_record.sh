10f051e90d347a3ccdf35881e1bd7231307ccfe7
RenFukatsu <fukatsuren88@gmail.com>
2021-06-03 15:17:07 +0900

    catkin pkg : launch, udev, tools

--- tools/bag_record.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -ne 1 ] ; then
-    echo "Usage: $0 [bagfile_name]";
-    exit 1
-fi
-
-rosbag record /camera/aligned_depth_to_color/image_raw/compressed /camera/color/camera_info /camera/color/image_rect_color/compressed /theta_s/image_raw/compressed /roomba/odometry /scan -o $1
-
